fdfd916e4e11169f6ade3b93e9dba40c0c1ac57b
crueter <crueter@eden-emu.dev>
2025-07-20 19:21:07 -0400

    clone 10 times max
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/clone.sh
+++ .ci/clone.sh
@@ -1,5 +1,13 @@
+TRIES=0
+
 while ! git clone $FORGEJO_CLONE_URL eden; do
   echo "Clone failed!"
+  TRIES=$(($TRIES + 1))
+  if [ "$TRIES" = 10 ]; then
+    echo "Failed to clone after ten tries. Exiting."
+    exit 1
+  fi
+
   sleep 5
   echo "Trying clone again..."
   rm -rf ./eden || true
