39f0d25a1847abea44666e673a88ffbb60fd2e53
crueter <crueter@crueter.xyz>
2025-09-07 22:46:32 -0400

    refactor: clang-cl and windows arm
    Signed-off-by: crueter <crueter@crueter.xyz>

--- .ci/windows/build.sh
+++ .ci/windows/build.sh
@@ -1,50 +1,20 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-# SPDX-FileCopyrightText: 2025 eden Emulator Project
+# SPDX-FileCopyrightText: 2025 Eden Emulator Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 cd eden
 
-if [ "$DEVEL" != "true" ]; then
-    export EXTRA_CMAKE_FLAGS=("${EXTRA_CMAKE_FLAGS[@]}" -DENABLE_QT_UPDATE_CHECKER=ON)
-fi
+[ "$COMPILER" == "clang" ] && EXTRA_CMAKE_FLAGS+=(-DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl)
+[ -z "$WINDEPLOYQT" ] && { echo "WINDEPLOYQT environment variable required."; exit 1; }
 
-if [ "$CCACHE" = "true" ]; then
-    export EXTRA_CMAKE_FLAGS=("${EXTRA_CMAKE_FLAGS[@]}" -DUSE_CCACHE=ON)
-fi
+EXTRA_CMAKE_FLAGS+=($@)
 
-if [ "$BUNDLE_QT" = "true" ]; then
-    export EXTRA_CMAKE_FLAGS=("${EXTRA_CMAKE_FLAGS[@]}" -DYUZU_USE_BUNDLED_QT=ON)
-else
-    export EXTRA_CMAKE_FLAGS=("${EXTRA_CMAKE_FLAGS[@]}" -DYUZU_USE_BUNDLED_QT=OFF)
-fi
-
-if [ -z "$BUILD_TYPE" ]; then
-    export BUILD_TYPE="Release"
-fi
-
-if [ "$WINDEPLOYQT" = "" ]; then
-    echo "You must supply the WINDEPLOYQT environment variable."
-    exit 1
-fi
-
-if [ "$USE_WEBENGINE" = "true" ]; then
-    WEBENGINE=ON
-else
-    WEBENGINE=OFF
-fi
-
-if [ "$USE_MULTIMEDIA" = "false" ]; then
-    MULTIMEDIA=OFF
-else
-    MULTIMEDIA=ON
-fi
-
-export EXTRA_CMAKE_FLAGS=("${EXTRA_CMAKE_FLAGS[@]}" $@)
+echo $EXTRA_CMAKE_FLAGS
 
 mkdir -p build && cd build
 cmake .. -G Ninja \
-    -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+    -DCMAKE_BUILD_TYPE="${BUILD_TYPE:-Release}" \
 	-DENABLE_QT_TRANSLATION=ON \
     -DUSE_DISCORD_PRESENCE=ON \
     -DYUZU_USE_BUNDLED_SDL2=ON \
@@ -53,11 +23,14 @@ cmake .. -G Ninja \
     -DDYNARMIC_TESTS=OFF \
     -DYUZU_CMD=OFF \
     -DYUZU_ROOM_STANDALONE=OFF \
-    -DYUZU_USE_QT_MULTIMEDIA=$MULTIMEDIA \
-    -DYUZU_USE_QT_WEB_ENGINE=$WEBENGINE \
+    -DYUZU_USE_QT_MULTIMEDIA=${USE_MULTIMEDIA:-false} \
+    -DYUZU_USE_QT_WEB_ENGINE=${USE_WEBENGINE:-false} \
     -DYUZU_ENABLE_LTO=ON \
 	-DCMAKE_EXE_LINKER_FLAGS=" /LTCG" \
     -DDYNARMIC_ENABLE_LTO=ON \
+    -DYUZU_USE_BUNDLED_QT=${BUNDLE_QT:-false} \
+    -DUSE_CCACHE=${CCACHE:-false} \
+    -DENABLE_QT_UPDATE_CHECKER=${DEVEL:-true} \
     "${EXTRA_CMAKE_FLAGS[@]}"
 
 ninja
