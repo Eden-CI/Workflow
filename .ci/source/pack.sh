430f7b80d76df43b7fab89bc2416557cfb9943e9
crueter <crueter@eden-emu.dev>
2025-09-17 14:02:24 -0400

    oopsies
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/source/pack.sh
+++ .ci/source/pack.sh
@@ -4,7 +4,7 @@ export ARCHIVE="git-archive-all-1.23.1"
 wget https://github.com/Kentzo/git-archive-all/releases/download/1.23.1/$ARCHIVE.tar.gz -nv
 tar xf $ARCHIVE.tar.gz
 
-python ../$ARCHIVE/git_archive_all.py --include .cache --include GIT-RELEASE --include GIT-COMMIT --include GIT-TAG --include GIT-REFSPEC --force-submodules source.tar
+python $ARCHIVE/git_archive_all.py --include .cache --include GIT-RELEASE --include GIT-COMMIT --include GIT-TAG --include GIT-REFSPEC --force-submodules source.tar
 
 zstd -10 source.tar
 rm source.tar
