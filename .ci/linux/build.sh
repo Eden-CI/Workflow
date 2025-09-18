4e10615be8930d10f8f83f447ab0500c89f24641
crueter <crueter@eden-emu.dev>
2025-09-18 09:56:20 -0400

    fix fr
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/linux/build.sh
+++ .ci/linux/build.sh
@@ -8,43 +8,43 @@ amd64 | "")
     echo "Making amd64-v3 optimized build of Eden"
     ARCH="amd64_v3"
     ARCH_FLAGS="-march=x86-64-v3"
-    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=v3 -DYUZU_USE_EXTERNAL_SDL2=OFF -DYUZU_USE_BUNDLED_SDL2=ON -DYUZU_USE_BUNDLED_OPENSSL=ON)
+    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=v3 -DYUZU_USE_EXTERNAL_SDL2=OFF -DYUZU_USE_BUNDLED_SDL2=ON)
     ;;
 steamdeck | zen2)
     echo "Making Steam Deck (Zen 2) optimized build of Eden"
     ARCH="steamdeck"
     ARCH_FLAGS="-march=znver2 -mtune=znver2"
-    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=zen2 -DYUZU_SYSTEM_PROFILE=steamdeck -DYUZU_USE_EXTERNAL_SDL2=ON -DYUZU_USE_BUNDLED_OPENSSL=ON)
+    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=zen2 -DYUZU_SYSTEM_PROFILE=steamdeck -DYUZU_USE_EXTERNAL_SDL2=ON)
     ;;
 rog-ally | allyx | zen4)
     echo "Making ROG Ally X (Zen 4) optimized build of Eden"
     ARCH="rog-ally-x"
     ARCH_FLAGS="-march=znver4 -mtune=znver4"
-    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=zen2 -DYUZU_SYSTEM_PROFILE=steamdeck -DYUZU_USE_EXTERNAL_SDL2=ON -DYUZU_USE_BUNDLED_OPENSSL=ON)
+    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=zen2 -DYUZU_SYSTEM_PROFILE=steamdeck -DYUZU_USE_EXTERNAL_SDL2=ON)
     ;;
 legacy)
     echo "Making amd64 generic build of Eden"
     ARCH=amd64
     ARCH_FLAGS="-march=x86-64 -mtune=generic"
-    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=generic -DYUZU_USE_BUNDLED_SDL2=OFF -DYUZU_USE_EXTERNAL_SDL2=OFF -DYUZU_USE_BUNDLED_SDL2=ON -DYUZU_USE_BUNDLED_OPENSSL=ON)
+    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=generic -DYUZU_USE_EXTERNAL_SDL2=OFF -DYUZU_USE_BUNDLED_SDL2=ON)
     ;;
 aarch64)
     echo "Making armv8-a build of Eden"
     ARCH=aarch64
-    ARCH_FLAGS="-march=armv8-a -mtune=generic -w"
-    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=generic -DYUZU_USE_EXTERNAL_SDL2=OFF -DYUZU_USE_BUNDLED_SDL2=ON -DYUZU_USE_BUNDLED_OPENSSL=ON)
+    ARCH_FLAGS="-march=armv8-a -mtune=generic"
+    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=generic -DYUZU_USE_EXTERNAL_SDL2=OFF -DYUZU_USE_BUNDLED_SDL2=ON)
     ;;
 armv9)
     echo "Making armv9-a build of Eden"
     ARCH=armv9
-    ARCH_FLAGS="-march=armv9-a -mtune=generic -w"
-    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=armv9 -DYUZU_USE_EXTERNAL_SDL2=OFF -DYUZU_USE_BUNDLED_SDL2=ON -DYUZU_USE_BUNDLED_OPENSSL=ON)
+    ARCH_FLAGS="-march=armv9-a -mtune=generic"
+    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=armv9 -DYUZU_USE_EXTERNAL_SDL2=OFF -DYUZU_USE_BUNDLED_SDL2=ON)
     ;;
 native)
     echo "Making native build of Eden"
     ARCH="$(uname -m)"
     ARCH_FLAGS="-march=native -mtune=native"
-    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=native -DYUZU_USE_EXTERNAL_SDL2=OFF -DYUZU_USE_BUNDLED_SDL2=ON -DYUZU_USE_BUNDLED_OPENSSL=ON)
+    export EXTRA_CMAKE_FLAGS=(-DYUZU_BUILD_PRESET=native -DYUZU_USE_EXTERNAL_SDL2=OFF -DYUZU_USE_BUNDLED_SDL2=ON)
     ;;
 *)
     echo "Invalid target $1 specified, must be one of native, amd64, steamdeck, zen2, allyx, rog-ally, zen4, legacy, aarch64, armv9"
@@ -60,9 +60,21 @@ else
     EXTRA_CMAKE_FLAGS+=(-DYUZU_USE_PRECOMPILED_HEADERS=OFF)
 fi
 
-[ "$COMPILER" = "clang" ] && EXTRA_CMAKE_FLAGS+=(-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++) && ARCH_FLAGS="$ARCH_FLAGS -fuse-ld=lld"
+if [ "$COMPILER" = "clang" ]; then
+    EXTRA_CMAKE_FLAGS+=(-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++)
+    ARCH_FLAGS="$ARCH_FLAGS -fuse-ld=lld -w"
+else
+    EXTRA_CMAKE_FLAGS+=(-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++)
+    ARCH_FLAGS="$ARCH_FLAGS -fuse-ld=mold -w"
+fi
 
 [ "$DEVEL" != "true" ] && EXTRA_CMAKE_FLAGS+=(-DENABLE_QT_UPDATE_CHECKER=ON)
+
+if [ "$SDL2" = "bundled" ]; then
+    EXTRA_CMAKE_FLAGS+=(-DYUZU_USE_BUNDLED_SDL2=ON -DYUZU_USE_EXTERNAL_SDL2=OFF)
+else
+    EXTRA_CMAKE_FLAGS+=(-DYUZU_USE_BUNDLED_SDL2=OFF -DYUZU_USE_EXTERNAL_SDL2=ON)
+fi
 
 EXTRA_CMAKE_FLAGS+=("$@")
 
@@ -81,9 +93,9 @@ cmake .. -G Ninja \
     -DYUZU_USE_BUNDLED_FFMPEG=ON \
     -DYUZU_USE_QT_MULTIMEDIA=${MULTIMEDIA:-OFF} \
     -DYUZU_USE_QT_WEB_ENGINE=${WEBENGINE:-OFF} \
-    -DYUZU_USE_FASTER_LD=ON \
     -DYUZU_ENABLE_LTO=ON \
     -DDYNARMIC_ENABLE_LTO=ON \
+    -DYUZU_USE_BUNDLED_OPENSSL=ON \
     "${EXTRA_CMAKE_FLAGS[@]}"
 
 ninja -j$(nproc)
