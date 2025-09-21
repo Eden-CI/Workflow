d3995c63ee844d98eab7c82c701b54b7ba379cdb
crueter <crueter@eden-emu.dev>
2025-09-21 01:26:14 -0400

    [ci] Initial macOS builds, v3
    Signed-off-by: crueter <crueter@eden-emu.dev>

---
+++ .ci/macos/deps.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+brew install --formula --quiet \
+  autoconf \
+  automake \
+  boost \
+  ffmpeg \
+  fmt \
+  glslang \
+  hidapi \
+  libtool \
+  libusb \
+  lz4 \
+  ninja \
+  nlohmann-json \
+  openssl \
+  pkg-config \
+  sdl2 \
+  speexdsp \
+  zlib \
+  zstd \
+  cmake \
+  Catch2 \
+  molten-vk \
+  vulkan-loader \
+  spirv-tools \
+  mbedtls \
+  cubeb \
+  opus \
+  enet \
+  spirv-headers \
+  vulkan-headers
