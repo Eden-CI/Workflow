999f941ee2154037a95fee603775074b790a2ef8
crueter <crueter@eden-emu.dev>
2025-09-24 19:34:17 -0400

    try this?
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/macos/package.sh
+++ .ci/macos/package.sh
@@ -7,24 +7,26 @@ cd build/bin
 APP=eden.app
 
 macdeployqt "$APP" -verbose=2
-macdeployqt "$APP" -always-overwrite -verbose=2
+# macdeployqt "$APP" -always-overwrite -verbose=2
 
-# FixMachOLibraryPaths
-find "$APP/Contents/Frameworks" ""$APP/Contents/MacOS"" -type f \( -name "*.dylib" -o -perm +111 -a -not -name "*Qt*" \) | while read file; do
-    if file "$file" | grep -q "Mach-O"; then
-        otool -L "$file" | awk '/@rpath\// {print $1}' | while read lib; do
-            lib_name="${lib##*/}"
-            new_path="@executable_path/../Frameworks/$lib_name"
-            install_name_tool -change "$lib" "$new_path" "$file"
-        done
+# maybe unused
+FixMachOLibraryPaths() {
+    find "$APP/Contents/Frameworks" ""$APP/Contents/MacOS"" -type f \( -name "*.dylib" -o -perm +111 -a -not -name "*Qt*" \) | while read file; do
+        if file "$file" | grep -q "Mach-O"; then
+            otool -L "$file" | awk '/@rpath\// {print $1}' | while read lib; do
+                lib_name="${lib##*/}"
+                new_path="@executable_path/../Frameworks/$lib_name"
+                install_name_tool -change "$lib" "$new_path" "$file"
+            done
 
-        if [[ "$file" == *.dylib ]]; then
-            lib_name="${file##*/}"
-            new_id="@executable_path/../Frameworks/$lib_name"
-            install_name_tool -id "$new_id" "$file"
+            if [[ "$file" == *.dylib ]]; then
+                lib_name="${file##*/}"
+                new_id="@executable_path/../Frameworks/$lib_name"
+                install_name_tool -id "$new_id" "$file"
+            fi
         fi
-    fi
-done
+    done
+}
 
 codesign --deep --force --verbose --sign - "$APP"
 
