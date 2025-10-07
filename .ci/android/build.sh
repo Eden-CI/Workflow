7dd071d97da2d9722c27a2f79134d52f179a2547
crueter <crueter@eden-emu.dev>
2025-10-07 18:56:52 -0400

    no ccache
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/android/build.sh
+++ .ci/android/build.sh
@@ -3,9 +3,6 @@
 # SPDX-FileCopyrightText: 2025 Eden Emulator Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-NDK_CCACHE=$(which ccache)
-export NDK_CCACHE
-
 if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
     export ANDROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/ks.jks"
     base64 --decode <<< "${ANDROID_KEYSTORE_B64}" > "${ANDROID_KEYSTORE_FILE}"
--- .ci/android/deps.sh
+++ .ci/android/deps.sh
@@ -1,2 +1,7 @@
+#!/bin/sh -e
+
+# Explicitly disable man-db auto update (takes forever lol)
+sudo rm /var/lib/man-db/auto-update
+
 sudo apt-get update -y
-sudo apt-get install ccache apksigner glslang-tools libvulkan-dev python3-requests -y
+sudo apt-get install apksigner glslang-tools libvulkan-dev python3-requests -y
