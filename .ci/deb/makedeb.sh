023dd3f49db592c82fc64887a34c0325ddc97efa
crueter <crueter@eden-emu.dev>
2025-10-16 19:07:39 -0400

    asciidoctor
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/deb/makedeb.sh
+++ .ci/deb/makedeb.sh
@@ -1,10 +1,14 @@
 #!/bin/sh -ex
 
+MANDB=/var/lib/man-db/auto-update
+
+[ -f $MANDB ] && sudo rm $MANDB
+
 [ ! -d makedeb ] && git clone 'https://github.com/makedeb/makedeb' makedeb-src
 cd makedeb-src
 git checkout stable
 
-sudo apt install -y binutils build-essential curl fakeroot file \
+sudo apt install -y asciidoctor binutils build-essential curl fakeroot file \
 	gettext gawk libarchive-tools lsb-release python3 python3-apt zstd
 
 make prepare VERSION=16.0.0 RELEASE=stable TARGET=apt CURRENT_VERSION=16.0.0
