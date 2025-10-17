b85ba6f2e043d12e3af9b7f5d310c16f06a23a14
crueter <crueter@eden-emu.dev>
2025-10-16 23:18:33 -0400

    let's try this?
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/deb/makedeb.sh
+++ .ci/deb/makedeb.sh
@@ -1,30 +1,12 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 
-MANDB=/var/lib/man-db/auto-update
-
-ROOTDIR="$PWD"
-
-# containers don't need sudo
+# if in a container (does not have sudo), install sudo and make a build user
 if command -v sudo > /dev/null ; then
-	SUDO=sudo
+	useradd -m -s /bin/bash -d /build build
+	echo "build ALL=NOPASSWD: ALL" >> /etc/sudoers
+	apt install sudo
+	exec su - build -c ".ci/deb/build.sh"
+# otherwise just run normally
 else
-	SUDO=""
+	.ci/deb/build.sh
 fi
-
-[ -f $MANDB ] && $SUDO rm $MANDB
-
-[ ! -d makedeb-src ] && git clone 'https://github.com/makedeb/makedeb' makedeb-src
-cd makedeb-src
-git checkout stable
-
-if command -v apt > /dev/null; then
-	$SUDO apt update
-	$SUDO apt install -y asciidoctor binutils build-essential curl fakeroot file \
-		gettext gawk libarchive-tools lsb-release python3 python3-apt zstd
-fi
-
-make prepare VERSION=16.0.0 RELEASE=stable TARGET=apt CURRENT_VERSION=16.0.0 FILESYSTEM_PREFIX="$ROOTDIR/makedeb"
-make
-make package DESTDIR="$ROOTDIR/makedeb" TARGET=apt
-
-[ -n "$GITHUB_PATH" ] && echo "$ROOTDIR/makedeb/usr/bin" >> "$GITHUB_PATH"
