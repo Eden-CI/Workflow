cda9bc4a70d3dc176e9666e29d711f0b90c4e29a
crueter <crueter@eden-emu.dev>
2025-10-17 03:32:02 -0400

    what
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/common/targets.sh
+++ .ci/common/targets.sh
@@ -81,7 +81,7 @@ if [ "$PLATFORM" = "linux" ] || [ "$COMPILER" = "clang" ]; then
 
 		PROFDATA="$PWD/eden.profdata"
 		[ -f "$PROFDATA" ] && rm -f "$PROFDATA"
-		curl -L https://github.com/Eden-CI/PGO/releases/latest/download/eden.profdata -o eden.profdata
+		curl -L https://github.com/Eden-CI/PGO/releases/latest/download/eden.profdata -o "$PROFDATA"
 		ARCH_FLAGS="${ARCH_FLAGS} -fprofile-use=$PROFDATA -Wno-backend-plugin -Wno-profile-instr-unprofiled -Wno-profile-instr-out-of-date"
 	fi
 fi
