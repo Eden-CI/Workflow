0a9cb84fef1ad593d098c614893e03c6ae9a5bb5
crueter <crueter@eden-emu.dev>
2025-10-16 23:21:32 -0400

    ??
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/deb/makedeb.sh
+++ .ci/deb/makedeb.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
 
 # if in a container (does not have sudo), install sudo and make a build user
-if command -v sudo > /dev/null ; then
+if ! command -v sudo > /dev/null ; then
 	useradd -m -s /bin/bash -d /build build
 	echo "build ALL=NOPASSWD: ALL" >> /etc/sudoers
 	apt install sudo
-	exec su - build -c ".ci/deb/build.sh"
+	sudo -u build -c ".ci/deb/build.sh"
 # otherwise just run normally
 else
 	.ci/deb/build.sh
