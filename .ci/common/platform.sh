999db4e8ede0d62c28bc4747090cda1d3d4d7de8
crueter <crueter@eden-emu.dev>
2025-11-01 03:19:47 -0400

    fixup and formatting
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/common/platform.sh
+++ .ci/common/platform.sh
@@ -7,13 +7,23 @@
 
 uname -s
 
-case "$(uname -s)" in
+# special case for MSYS2
+if [ ! -z "$MSYSTEM" ]; then
+	PLATFORM=msys
+	STANDALONE=ON
+	OPENSSL=OFF
+	FFMPEG=ON
+	BUNDLED=OFF
+	SUPPORTS_TARGETS=ON
+else
+
+	case "$(uname -s)" in
 	Linux*)
 		PLATFORM=linux
 		STANDALONE=OFF
 		FFMPEG=ON
 		OPENSSL=ON
-        SUPPORTS_TARGETS=ON
+		SUPPORTS_TARGETS=ON
 		;;
 	Darwin*)
 		PLATFORM=macos
@@ -22,34 +32,29 @@ case "$(uname -s)" in
 		OPENSSL=OFF
 		export LIBVULKAN_PATH="/opt/homebrew/lib/libvulkan.1.dylib"
 		;;
-	CYGWIN*|MINGW*)
+	CYGWIN* | MINGW* | MSYS*)
 		PLATFORM=win
 		STANDALONE=ON
 		OPENSSL=ON
 		FFMPEG=ON
-        [ "$COMPILER" = "clang" ] && SUPPORTS_TARGETS=ON
+		[ "$COMPILER" = "clang" ] && SUPPORTS_TARGETS=ON
 
 		# LTO is completely broken on MSVC
 		LTO=off
 		;;
-    MSYS*)
-        PLATFORM=msys
-        STANDALONE=ON
-        OPENSSL=ON
-        FFMPEG=ON
-        SUPPORTS_TARGETS=ON
-        ;;
 	FreeBSD*)
 		PLATFORM=freebsd
 		STANDALONE=OFF
 		FFMPEG=OFF
 		OPENSSL=ON
-        SUPPORTS_TARGETS=ON
+		SUPPORTS_TARGETS=ON
 		;;
 	*)
 		echo "Unknown platform $(uname -s)"
-		exit 1 ;;
-esac
+		exit 1
+		;;
+	esac
+fi
 
 export PLATFORM
 export STANDALONE
@@ -57,3 +62,6 @@ export LTO
 export FFMPEG
 export OPENSSL
 export SUPPORTS_TARGETS
+export BUNDLED
+
+# TODO(crueter): document outputs n such
