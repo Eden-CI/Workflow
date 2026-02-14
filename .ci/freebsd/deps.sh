80c43c2dea421e823473b1ad44d999ca954b2c6f
crueter <crueter@eden-emu.dev>
2026-02-13 21:59:43 -0500

    Bump cache
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/freebsd/deps.sh
+++ .ci/freebsd/deps.sh
@@ -16,7 +16,7 @@ pkg install -y \
   devel/pkgconf \
   devel/qt6-base \
   devel/qt6-tools \
-  devel/qt6-charts \
+  x11-toolkits/qt6-charts \
   devel/simpleini \
   net/enet \
   multimedia/ffnvcodec-headers \
