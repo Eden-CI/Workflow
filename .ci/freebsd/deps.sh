c7bd7d889a3ef6c1e32576ea61f19b519c7594a6
Ploo <239304139+xinitrcn1@users.noreply.github.com>
2026-04-17 04:40:23 +0000

    Update deps.sh

--- .ci/freebsd/deps.sh
+++ .ci/freebsd/deps.sh
@@ -3,6 +3,9 @@
 # SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+# required because VM on frebsd actions is outdated as shit
+pkg update -y
+pkg upgrade -y
 pkg install -y \
   devel/cmake \
   devel/sdl20 \
